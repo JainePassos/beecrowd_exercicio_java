f4ceac9943a85e86aa6e780a5e78fa35e59a2777
jainepassos91@gmail.com <jainepassos91@gmail.com>
2026-09-04 11:53:22 -0300

    Exercicio 1095

---
+++ src/ex1095/SequenciaIJ1.java
@@ -0,0 +1,13 @@
+package ex1095;
+
+public class SequenciaIJ1 {
+    public static void main(String[] args) {
+        int I = 1, J = 60;
+        while (I < 60 && J >=0 ){
+            System.out.println("I="+I+" " + "J=" + J);
+            I+=3;
+            J-=5;
+        }
+
+    }
+}
