8a30f8d2ec1951abff7719efadbbdd8a14fd5fb3
jainepassos91@gmail.com <jainepassos91@gmail.com>
2026-09-09 12:23:40 -0300

    Exercicio 1096

---
+++ src/ex1096/SequenciaIJ2.java
@@ -0,0 +1,14 @@
+package ex1096;
+
+public class SequenciaIJ2 {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 9 ; i+=2) {
+            for (int I = i, J = 7; J >=5 ; J-=1) {
+                System.out.println("I="+I + " J="+J);
+            }
+        }
+
+
+    }
+}
