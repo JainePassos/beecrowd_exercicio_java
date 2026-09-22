98fa9e7f4587a87a74295cb9a2e09ca2904ad86a
jainepassos91@gmail.com <jainepassos91@gmail.com>
2026-09-22 19:24:36 -0300

    Atualiza exercício 1114

--- src/ex1114/SenhaFixa.java
+++ src/ex1114/SenhaFixa.java
@@ -1,4 +1,23 @@
 package ex1114;
 
+import java.util.Scanner;
+
 public class SenhaFixa {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int Senha = 2002;
+
+        for (int i = 0; i != Senha ; i++) {
+
+            int X = sc.nextInt();
+
+            if(Senha == X){
+                System.out.println("Acesso Permitido");
+                break;
+            }else {
+                System.out.println("Senha Invalida");
+            }
+
+        }
+    }
 }
--- src/ex1115/Quadrante.java
+++ src/ex1115/Quadrante.java
@@ -1,4 +1,34 @@
 package ex1115;
 
+import java.util.Scanner;
+
 public class Quadrante {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int X = 1;
+        int Y = 1;
+
+        int i = 0;
+        while (i != X || i != Y) {
+
+            X = sc.nextInt();
+            Y = sc.nextInt();
+
+            if (X == 0 || Y == 0){
+                break;
+           }else if (X > 0 && Y > 0 ) {
+                System.out.println("primeiro");
+            } else if (X < 0 && Y > 0) {
+                System.out.println("segundo");
+            } else if (X < 0 && Y < 0) {
+                System.out.println("terceiro");
+            }else if (X > 0 && Y < 0 ){
+                System.out.println("quarto");
+            }
+
+            i++;
+        }
+
+    }
 }
